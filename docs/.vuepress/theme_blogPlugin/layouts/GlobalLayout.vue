748a1fa70609dd76a4d371c171a09cfc2631d212
Heyff12 <645068564@qq.com>
2020-04-02 15:32:09 +0800

    uninstall useless plugins

---
+++ docs/.vuepress/theme_blogPlugin/layouts/GlobalLayout.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="global-layout">
+    <YaHeader />
+    <!-- <DefaultGlobalLayout/> -->
+    <section class="global-body">
+      <component :is="layout"/>
+    </section>
+    <YaFooter />
+  </div>
+</template>
+
+<script>
+import YaHeader from '../components/YaHeader'
+import YaFooter from '../components/YaFooter'
+import GlobalLayout from '@app/components/GlobalLayout.vue'
+export default {
+    name:'GlobalLayout',
+    components: {
+        YaHeader,
+        YaFooter,
+        DefaultGlobalLayout: GlobalLayout,
+    },
+    computed: {
+        layout () {
+        if (this.$page.path) {
+            if (this.$frontmatter.layout) {
+            // 你也可以像默认的 globalLayout 一样首先检测 layout 是否存在
+            return this.$frontmatter.layout
+            }
+            return 'Layout'
+        }
+        return 'NotFound'
+        }
+    }
+}
+</script>
+
+<style scoped>
+.global-body{
+  padding-top: 100px;
+}
+</style>>
