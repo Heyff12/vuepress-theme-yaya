2bedad8f324f80639b6d85444d132b4c254c239e
Heyff12 <645068564@qq.com>
2023-10-04 14:05:24 +0800

    set layout for page

---
+++ docs/works/2020-04-01-index_haha copy 5.md
@@ -0,0 +1,8 @@
+---
+title: '小程序路由4'
+# layout: 'List'
+tags:
+ - 网站
+---
+
+## reading
--- index.js
+++ index.js
@@ -1,91 +1,98 @@
 module.exports = (themeConfig, ctx) => {
-
-    const blogOptions = {
-        directories: [{
-                // Unique ID of current classification
-                id: 'study',
-                // Target directory
-                dirname: 'study',
-                // Path of the `entry page` (or `list page`)
-                path: '/study/',
-                // layout: 'Layout',
-                //   itemPermalink: '/:year/:month/:day/:slug',
-                itemPermalink: '/:regular',
-                pagination: {
-                    lengthPerPage: 10,
-                },
-            },
-            {
-                id: 'works',
-                dirname: 'works',
-                path: '/works/',
-                itemPermalink: '/:regular',
-                layout: 'LayoutWork',
-                pagination: {
-                    lengthPerPage: 6,
-                },
-            },
-            {
-                id: 'reading',
-                dirname: 'reading',
-                path: '/reading/',
-                itemPermalink: '/:regular',
-                layout: 'LayoutBook',
-                pagination: {
-                    lengthPerPage: 9,
-                },
-            },
-            {
-                id: 'writing',
-                dirname: 'writing',
-                path: '/writing/',
-                itemPermalink: '/:regular',
-                layout: 'LayoutPic',
-                pagination: {
-                    lengthPerPage: 8,
-                },
-            },
-        ],
-        frontmatters: [{
-            id: "tags",
-            keys: ['tags'],
-            path: '/tags/',
-            layout: 'FrontmatterKey', //defaults to `FrontmatterKey.vue`
-            frontmatter: {
-                title: '分类检索文章'
-            },
-            // itemlayout: 'Layout',
-        }],
-    }
-
-    const plugins = [
-        ['@vuepress/back-to-top'],
-        [
-            '@vuepress/last-updated',
-            {
-                transformer: (timestamp, lang) => {
-                    const moment = require('moment')
-                    moment.locale(lang)
-                    return moment().format('YYYY-MM-D')
-                }
-            }
-        ],
-        ['@vuepress/plugin-nprogress'],
-        ['@vuepress/search', {
-            searchMaxSuggestions: 6
-        }],
-        ['@vuepress/blog', blogOptions],
-    ]
-
-    const config = {
-        extend: '@vuepress/theme-default',
-        globalLayout: '/layouts/GlobalLayout.vue',
-        markdown: {
-            lineNumbers: true,
-            extractHeaders: ['h1', 'h2', 'h3', 'h4']
+  const blogOptions = {
+    directories: [
+      {
+        // Unique ID of current classification
+        id: "study",
+        // Target directory
+        dirname: "study",
+        // Path of the `entry page` (or `list page`)
+        path: "/study/",
+        // layout: 'Layout',
+        //   itemPermalink: '/:year/:month/:day/:slug',
+        itemPermalink: "/:regular",
+        pagination: {
+          lengthPerPage: 10,
         },
-        plugins,
-    }
+      },
+      {
+        id: "works",
+        dirname: "works",
+        path: "/works/",
+        itemPermalink: "/:regular",
+        layout: "LayoutWork",
+        // itemLayout: 'LayoutWork', // layout for detail page
+        pagination: {
+          lengthPerPage: 6,
+          layout: "LayoutWork", // layout for page
+        },
+      },
+      {
+        id: "reading",
+        dirname: "reading",
+        path: "/reading/",
+        itemPermalink: "/:regular",
+        layout: "LayoutBook",
+        pagination: {
+          lengthPerPage: 9,
+        },
+      },
+      {
+        id: "writing",
+        dirname: "writing",
+        path: "/writing/",
+        itemPermalink: "/:regular",
+        layout: "LayoutPic",
+        pagination: {
+          lengthPerPage: 8,
+        },
+      },
+    ],
+    frontmatters: [
+      {
+        id: "tags",
+        keys: ["tags"],
+        path: "/tags/",
+        layout: "FrontmatterKey", //defaults to `FrontmatterKey.vue`
+        frontmatter: {
+          title: "分类检索文章",
+        },
+        // itemlayout: 'Layout',
+      },
+    ],
+  };
 
-    return config
-}
+  const plugins = [
+    ["@vuepress/back-to-top"],
+    [
+      "@vuepress/last-updated",
+      {
+        transformer: (timestamp, lang) => {
+          const moment = require("moment");
+          moment.locale(lang);
+          return moment().format("YYYY-MM-D");
+        },
+      },
+    ],
+    ["@vuepress/plugin-nprogress"],
+    [
+      "@vuepress/search",
+      {
+        searchMaxSuggestions: 6,
+      },
+    ],
+    ["@vuepress/blog", blogOptions],
+  ];
+
+  const config = {
+    extend: "@vuepress/theme-default",
+    globalLayout: "/layouts/GlobalLayout.vue",
+    markdown: {
+      lineNumbers: true,
+      extractHeaders: ["h1", "h2", "h3", "h4"],
+    },
+    plugins,
+  };
+
+  return config;
+};
